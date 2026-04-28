e89842e150796bcd1511094836feb76807f5f3ca
aman singh <2110aman@gmail.com>
2026-04-28 18:43:31 +0530

    feat: add remote pattern for Vultr Object Storage to image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cp-bucket.del1.vultrobjects.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
